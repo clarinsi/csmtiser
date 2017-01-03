850cafdfe9b5a4751df103675e8313add297cb85
Nikola Ljubešić <nljubesi@gmail.com>
2017-01-03 14:17:48 +0000

    Absolute paths added to training and tuning

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ for index,pth in enumerate(config.lms):
 
 sys.stdout.write('Building the untuned system\n')
 sys.stdout.flush()
-os.system(config.moses_scripts+'/training/train-model.perl -root-dir '+config.working_dir+' -corpus '+config.working_dir+'/train -f orig -e norm -alignment grow-diag-final-and -reordering msd-bidirectional-fe -lm 0:'+str(config.lm_order)+':'+config.working_dir+'/train.norm.blm:8 -cores '+str(config.num_cores)+' --mgiza -mgiza-cpus '+str(config.num_cores)+' -external-bin-dir '+config.mgiza+' >> '+config.working_dir+'/train.log 2>&1')
+os.system(config.moses_scripts+'/training/train-model.perl -root-dir '+config.working_dir+' -corpus '+config.working_dir+'/train -f orig -e norm -alignment grow-diag-final-and -lm 0:'+str(config.lm_order)+':'+config.working_dir+'/train.norm.blm:8 -cores '+str(config.num_cores)+' --mgiza -mgiza-cpus '+str(config.num_cores)+' -external-bin-dir '+config.mgiza+' >> '+config.working_dir+'/train.log 2>&1')
 
 sys.stdout.write('Updating the moses.ini file\n')
 ini=open(config.working_dir+'/model/moses.ini').read().replace('[distortion-limit]\n6','[distortion-limit]\n0')
@@ -39,4 +39,4 @@ for index in range(1,len(config.lms)):
 modini.close()
 sys.stdout.write('Tuning the system\n')
 sys.stdout.flush()
-os.system(config.moses_scripts+'training/mert-moses.pl dev.orig dev.norm '+config.moses+'/moses '+config.working_dir+'/model/moses.mod.ini --mertdir '+config.moses+' --working-dir '+config.working_dir+'/mert-work/ --mertargs="--sctype WER" --decoder-flags="-threads '+str(config.num_cores)+'" >> train.log 2>&1')
+os.system(config.moses_scripts+'training/mert-moses.pl '+config.working_dir+'/dev.orig '+config.working_dir+'/dev.norm '+config.moses+'/moses '+config.working_dir+'/model/moses.mod.ini --mertdir '+config.moses+' --working-dir '+config.working_dir+'/mert-work/ --mertargs="--sctype WER" --decoder-flags="-threads '+str(config.num_cores)+'" >> train.log 2>&1')
